da2046ef0b224bf478b9c2e14fc7de45756052e7
DESKTOP-MRQNKBH\mazur <mazurchuk@gmail.com>
2023-05-20 00:53:32 +0300

    PDU Address

---
+++ PDUAddress.java
@@ -0,0 +1,70 @@
+public class PDUAddress {
+
+    public static final int ADDRESS_TYPE_MASK          = 0x70; // 01110000
+	public static final int ADDRESS_TYPE_UNKNOWN       = 0x00;
+	public static final int ADDRESS_TYPE_INTERNATIONAL = 0x10; // 00010000
+    public static final int ADDRESS_TYPE_NATIONAL      = 0x20; // 00100000
+	public static final int ADDRESS_TYPE_ALPHANUMERIC = 0x50;  // 01110000
+    public static final String ADDRESS_INTR_HEADER = "91";
+    private String Number;
+    private Boolean International;
+
+    public PDUAddress(String address){
+
+        this.Number = address;
+        this.International = address.startsWith("+");
+        if (this.International)
+		{
+			this.Number = address.substring(1);
+		}
+    }
+    public String Encode(){
+        // Convert the number to ASCII
+        String result = Long.toHexString(Long.parseLong(this.Number));
+
+        // Add the "91" prefix which indicates an international format number
+        if(this.International){
+            result = ADDRESS_INTR_HEADER + result;
+        }
+
+        // lengh byte
+        var len = result.length() / 2;
+        String lengthByte = Integer.toHexString(len);
+        if(len < 10){
+            lengthByte = "0"+lengthByte;
+        }
+        // Swap the characters
+        String swapped = swapCharacters(result);
+
+        return lengthByte + swapped;
+
+    }
+
+    public String Decode(){
+
+        // skip the length
+        String address = this.Number.substring(2);
+
+        String result = swapCharacters(address);
+        // check if international
+        this.International = result.startsWith(ADDRESS_INTR_HEADER);
+        if(this.International ){
+            result = result.substring(ADDRESS_INTR_HEADER.length());
+        }
+        result = String.valueOf(Long.parseLong(result, 16));
+        if(this.International){
+            result = "+" + result;
+        }
+        return result;
+    }
+
+    private String swapCharacters(String address) {
+        StringBuilder swapped = new StringBuilder();
+
+        for (int i = 0; i < address.length() - 1; i += 2) {
+            swapped.append(address.charAt(i + 1)).append(address.charAt(i));
+        }
+
+        return swapped.toString();
+    }
+}
--- tmp/Test_02.java
+++ tmp/Test_02.java
@@ -1,5 +1,20 @@
 public class Test_02 {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
+        TestAddress("1234567890");
+        TestAddress("+80935430492");
+       }
+    
+       private static void TestAddress(String address){
+    
+        PDUAddress address1 = new PDUAddress(address);    
+        String encoded = address1.Encode();
+        PDUAddress address2 = new PDUAddress(encoded);    
+        String decoded = address2.Decode();
+        System.out.println("SRC = " + address + " DST = " + decoded);
+    
+       }
+
+   public static void main2(String[] args) {
     String str = "852 90288000";
     System.out.println(encodeMessage(str));
    }
